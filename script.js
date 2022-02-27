5a239a0128c0588b2cca3d1e91c5dc11adf54798
AbhinavRattan27 <83867719+AbhinavRattan27@users.noreply.github.com>
2022-02-27 13:51:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function button1js(){
+    window.location.replace("https://abhinavrattan27.github.io/main-game-1st-/");
+}
+
+function button2js(){
+    document.getElementById("bye").innerHTML="Bye!"
+    window.location.replace("https://www.google.com/");
+}
